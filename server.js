6ec294087f32aaa44a3427335d60fd6346c6fc38
Xavier Torres-Casso <xavier@Xaviers-Air.charter.com>
2021-03-26 16:34:31 -0700

    Step 15

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ var session = require('express-session');
 var passport = require('passport');
 var methodOverride= require('method-override');
 
+
 require('dotenv').config();
 require('./config/database');
+
 require('./config/passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/beers');
 
 var app = express();
 
@@ -36,13 +38,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 app.use(function(req, res, next) {
   res.locals.user = req.user;
   next();
 });
+
+app.use('/', indexRouter);
+app.use('/beers', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
