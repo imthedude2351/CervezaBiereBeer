b7eb0913d5684fbc7f2c4fb9e27a9c511203f133
Xavier Torres-Casso <xavier@Xaviers-Air.charter.com>
2021-03-30 15:24:17 -0700

    delete function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -19,13 +19,11 @@ function update(req, res) {
 function deleteReview(req, res, next) {
   Beer.findOne({'reviews._id': req.params.id}).then(function(beer) {
     const review = beer.reviews.id(req.params.id);
-    // ensure the review was created by the logged in user
     if(!review.user.equals(req.user._id)) return res.redirect(`/beers/${beer._id}`);
     review.remove();
     beer.save().then(function() {
       res.redirect(`/beers/${beer._id}`);
     }).catch(function(err){
-      // Let Express display an error
       return next(err);
       // Another option
       // res.redirect(`/beers/${beer._id}`);
@@ -35,7 +33,6 @@ function deleteReview(req, res, next) {
 
 function create(req, res) {
   Beer.findById(req.params.id, function(err, beer) {
-    // Add the user-centric info to req.body (the new review)
     req.body.user = req.user._id;
     req.body.userName = req.user.name;
     req.body.userAvatar = req.user.avatar;
